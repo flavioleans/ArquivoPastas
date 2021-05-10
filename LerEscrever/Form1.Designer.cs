90a4fa0b9a5ddca19390dd71e80ff9cce1ffa7b5
flavioleans <leans.flavio@gmail.com>
2021-05-09 21:51:41 -0300

    Completando os estudos de arquivos

---
+++ LerEscrever/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace LerEscrever
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEscrever = new System.Windows.Forms.Button();
+            this.btnLerTxt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnLerBinario = new System.Windows.Forms.Button();
+            this.btnEscreverBinario = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEscrever
+            // 
+            this.btnEscrever.Location = new System.Drawing.Point(12, 299);
+            this.btnEscrever.Name = "btnEscrever";
+            this.btnEscrever.Size = new System.Drawing.Size(152, 49);
+            this.btnEscrever.TabIndex = 1;
+            this.btnEscrever.Text = "Escrever";
+            this.btnEscrever.UseVisualStyleBackColor = true;
+            this.btnEscrever.Click += new System.EventHandler(this.btnEscrever_Click);
+            // 
+            // btnLerTxt
+            // 
+            this.btnLerTxt.Location = new System.Drawing.Point(170, 299);
+            this.btnLerTxt.Name = "btnLerTxt";
+            this.btnLerTxt.Size = new System.Drawing.Size(152, 49);
+            this.btnLerTxt.TabIndex = 2;
+            this.btnLerTxt.Text = "Ler Txt";
+            this.btnLerTxt.UseVisualStyleBackColor = true;
+            this.btnLerTxt.Click += new System.EventHandler(this.btnLerTxt_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(638, 258);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "";
+            // 
+            // btnLerBinario
+            // 
+            this.btnLerBinario.Location = new System.Drawing.Point(328, 299);
+            this.btnLerBinario.Name = "btnLerBinario";
+            this.btnLerBinario.Size = new System.Drawing.Size(152, 49);
+            this.btnLerBinario.TabIndex = 4;
+            this.btnLerBinario.Text = "Ler Binario";
+            this.btnLerBinario.UseVisualStyleBackColor = true;
+            this.btnLerBinario.Click += new System.EventHandler(this.btnLerBinario_Click);
+            // 
+            // btnEscreverBinario
+            // 
+            this.btnEscreverBinario.Location = new System.Drawing.Point(486, 299);
+            this.btnEscreverBinario.Name = "btnEscreverBinario";
+            this.btnEscreverBinario.Size = new System.Drawing.Size(152, 49);
+            this.btnEscreverBinario.TabIndex = 5;
+            this.btnEscreverBinario.Text = "Escrever Binario";
+            this.btnEscreverBinario.UseVisualStyleBackColor = true;
+            this.btnEscreverBinario.Click += new System.EventHandler(this.btnEscreverBinario_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 387);
+            this.Controls.Add(this.btnEscreverBinario);
+            this.Controls.Add(this.btnLerBinario);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLerTxt);
+            this.Controls.Add(this.btnEscrever);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEscrever;
+        private System.Windows.Forms.Button btnLerTxt;
+        private System.Windows.Forms.RichTextBox textBox1;
+        private System.Windows.Forms.Button btnLerBinario;
+        private System.Windows.Forms.Button btnEscreverBinario;
+    }
+}
+
---
+++ LerEscrever/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LerEscrever
+{
+    public partial class Form1 : Form
+    {
+        byte[] buffer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Escrever
+        private void btnEscrever_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\ProjetosC#\teste.txt";
+            StreamWriter write = new StreamWriter(path, true, Encoding.UTF8);//treu aceita adicionar conteudo caso exista
+            string linha = textBox1.Text;
+            write.WriteLine(linha);
+
+            //write.Flush();
+            //write.Dispose();
+            write.Close();
+        }
+
+        //Ler
+        private void btnLerTxt_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            string path = @"D:\ProjetosC#\teste.txt";
+
+            StreamReader reader = new StreamReader(path);
+            //string txt = reader.ReadToEnd();// lê todo o conteudo
+
+            //Linha por linha
+            //string linha = reader.ReadLine();
+            //linha += reader.ReadLine();
+            //linha += reader.ReadLine();
+            //textBox1.Text = linha;
+
+            //Enquanto houver linha
+            //string linha = reader.ReadLine();
+            //while (linha != null)
+            //{
+            //    textBox1.Text += linha + "\n";
+            //    linha = reader.ReadLine();
+            //}
+
+            //ler caracter por caracter
+            while (!reader.EndOfStream)
+            {
+                textBox1.Text += (char)reader.Read();
+            }
+
+            reader.Close();
+        }
+
+        //Ler Binario
+        private void btnLerBinario_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\ProjetosC#\teste.txt";
+            FileStream file = File.OpenRead(path);
+            BinaryReader reader = new BinaryReader(file);
+
+            //metodo 1
+            //while (reader.BaseStream.Position != reader.BaseStream.Length)
+            //{
+            //    byte b = reader.ReadByte();
+            //    textBox1.Text += (char)b;
+            //}
+
+            //metodo 2
+            //buffer = reader.ReadBytes((int)reader.BaseStream.Length);
+            //foreach  (byte b in buffer)
+            //{
+            //    textBox1.Text += (char)b;
+            //}
+
+            //Metodo 3
+            buffer = File.ReadAllBytes(path);
+
+
+            reader.Close();
+        }
+
+        private void btnEscreverBinario_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\ProjetosC#\testeNovo.txt";
+            FileStream file = File.OpenWrite(path);
+            BinaryWriter writer = new BinaryWriter(file);
+            
+
+            writer.Write(buffer);
+
+            writer.Flush();
+            writer.Dispose();
+            writer.Close();
+        }
+    }
+}
